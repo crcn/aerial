54a808e24287b69dd0316c4b76018e5a94315f79
Craig Condon <craig.j.condon@gmail.com>
2017-06-16 07:14:02 -0400

    uncommented more tests

--- packages/aerial-synthetic-browser/src/test/standard/script-test.ts
+++ packages/aerial-synthetic-browser/src/test/standard/script-test.ts
@@ -1,42 +1,41 @@
-// import { expect } from "chai";
-// import { LogLevel } from "aerial-common";
-// import { SyntheticBrowser, SyntheticHTMLElement } from "aerial-synthetic-browser";
-// import { createTestMasterApplication } from "@tandem/editor/test";
-// import { loadTestBrowser } from "aerial-synthetic-browser/test";
+import { expect } from "chai";
+import { LogLevel } from "aerial-common";
+import { SyntheticBrowser, SyntheticHTMLElement } from "../..";
+import { loadTestBrowser } from "../utils";
 
-// // poorly organized DOM spec tests. TODO - move these into sep fiels
-// describe(__filename + "#", () => {
+// poorly organized DOM spec tests. TODO - move these into sep fiels
+describe(__filename + "#", () => {
 
-//   it("Can dynamically create a new script", async () => {
-//     const { window } = await loadTestBrowser({
-//       "index.html": `
-//         <span>
-//         </span>
-//         <script>
-//           var script = document.createElement("script");
-//           script.text = "document.querySelector('span').appendChild(document.createTextNode('a'))";
-//           document.appendChild(script);
-//         </script>
-//       `
-//     }, "index.html");
+  it("Can dynamically create a new script", async () => {
+    const { window } = await loadTestBrowser({
+      "index.html": `
+        <span>
+        </span>
+        <script>
+          var script = document.createElement("script");
+          script.text = "document.querySelector('span').appendChild(document.createTextNode('a'))";
+          document.appendChild(script);
+        </script>
+      `
+    }, "index.html");
 
-//     expect(window.document.querySelector("span").textContent).to.contain("a");
-//   });
+    expect(window.document.querySelector("span").textContent).to.contain("a");
+  });
 
-//   it("Can set the text of a script after it's been added to the DOM", async () => {
-//     const { window } = await loadTestBrowser({
-//       "index.html": `
-//         <span>
-//         </span>
-//         <script>
-//           var script = document.createElement("script");
-//           document.appendChild(script);
-//           script.text = "document.querySelector('span').appendChild(document.createTextNode('a'))";
-//         </script>
-//       `
-//     }, "index.html");
+  it("Can set the text of a script after it's been added to the DOM", async () => {
+    const { window } = await loadTestBrowser({
+      "index.html": `
+        <span>
+        </span>
+        <script>
+          var script = document.createElement("script");
+          document.appendChild(script);
+          script.text = "document.querySelector('span').appendChild(document.createTextNode('a'))";
+        </script>
+      `
+    }, "index.html");
 
-//     expect(window.document.querySelector("span").textContent).to.contain("a");
-//   });
+    expect(window.document.querySelector("span").textContent).to.contain("a");
+  });
 
-// });
+});
--- packages/aerial-synthetic-browser/src/test/standard/xml-http-request-test.ts
+++ packages/aerial-synthetic-browser/src/test/standard/xml-http-request-test.ts
@@ -1,15 +1,15 @@
-// import { expect } from "chai";
-// import { loadTestBrowser } from "../../test";
+import { expect } from "chai";
+import { loadTestBrowser } from "../../test";
 
-// describe(__filename + "#", () => {
-//   it("XMLHttpRequest exists", async () => {
-//     const { window } = await loadTestBrowser({
-//       "index.js": `
-//         document.body.appendChild(document.createTextNode(typeof XMLHttpRequest))
-//       `
-//     }, "index.js");
+describe(__filename + "#", () => {
+  it("XMLHttpRequest exists", async () => {
+    const { window } = await loadTestBrowser({
+      "index.js": `
+        document.body.appendChild(document.createTextNode(typeof XMLHttpRequest))
+      `
+    }, "index.js");
 
-//     expect(window.document.textContent).to.equal("function");
-//   });
+    expect(window.document.textContent).to.equal("function");
+  });
   
-// });
+});
