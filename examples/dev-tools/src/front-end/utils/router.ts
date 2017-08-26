727195efff11514820c560a6f319d71651777d2a
Craig Condon <craig.j.condon@gmail.com>
2017-08-26 13:07:18 -0400

    cancel task if running

--- examples/dev-tools/src/front-end/utils/router.ts
+++ examples/dev-tools/src/front-end/utils/router.ts
@@ -1,14 +1,21 @@
 import { RouterRootState } from "../state";
 import { LOCATION_CHANGED } from "../actions";
-import { select, fork, call } from "redux-saga/effects";
+import { select, fork, call, cancel } from "redux-saga/effects";
 
 export const getRouterLocation = (location: Location) => location.hash;
 
 export const whenLocation = (pattern: RegExp, fn: any, ...args: any[]) => fork(function*() {
     const state: RouterRootState = yield select();
+    let task;
+
     while(true) {
       if (state.router && pattern.test(state.router.location)) {
-        yield fork(fn, ...args);
+        if (!task) {
+          task = yield fork(fn, ...args);
+        }
+      } else if (task) {
+        yield cancel(task);
+        task = undefined;
       }
       yield take(LOCATION_CHANGED);
     }
